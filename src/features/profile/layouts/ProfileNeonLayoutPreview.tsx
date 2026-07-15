3346adf2de9fab686e8b344fee72d64ebf88710f
longern <i@longern.com>
2026-07-15 10:07:34 +0800

    Add neon layout

---
+++ src/features/profile/layouts/ProfileNeonLayoutPreview.tsx
@@ -0,0 +1,14 @@
+import "./ProfileNeonLayout.editor.css";
+
+export function ProfileNeonLayoutPreview() {
+  return (
+    <>
+      <span className="layout-preview-neon-avatar" />
+      <span className="layout-preview-neon-title" />
+      <span className="layout-preview-neon-subtitle" />
+      <span className="layout-preview-neon-link" />
+      <span className="layout-preview-neon-link" />
+      <span className="layout-preview-neon-link" />
+    </>
+  );
+}
